3aa6f707bf8b144385729e845f007c4ba160798a
i_mhmdw_i <mohamad3691379@yahoo.com>
2022-08-25 13:01:44 +0430

    first

---
+++ app.js
@@ -0,0 +1,44 @@
+                // path
+// const path = require("path")
+// console.log(path.parse(__filename))
+
+            // os
+// const os = require("os")
+// console.log(os.freemem())
+// console.log(os.totalmem())
+
+            //   fs
+// const fs = require('fs')
+// console.log(fs.readdirSync("./"))
+// console.log(fs.readdir("./",(err,files)=>{
+//     console.log(err)
+//     console.log(files)
+// }))
+
+        //   Event
+// const EventEmitter = require("events")      //class
+//  const emitter = new EventEmitter()
+//  emitter.on("bell",(e)=>{
+//     console.log(e)
+//     console.log("open the door")
+//  })
+//  emitter.emit("bell",{time:Date.now(),count:2})
+
+
+
+            // http
+// const http = require("http");
+// const server = http.createServer((req,res)=>{
+//     console.log("new conect Mohammad")
+//     if(req.url === "/"){
+//         res.write("home page")
+//         res.write(JSON.stringify({product: ["1pro","2pro"]}))
+//     }else{
+//         res.write("page more")
+//     }
+//     res.end()
+// });
+// server.listen(1379);
+
+
+
